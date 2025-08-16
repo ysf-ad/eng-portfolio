516e205df556cf27df99d9066b0f2ebeaadd7f8f
ysf-ad <yousifaldakoki@gmail.com>
2025-08-15 23:13:16 -0400

    Set light mode as default theme

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -23,7 +23,7 @@ import "./Main.scss";
 
 const Main = () => {
   const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
-  const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
+  const [isDark, setIsDark] = useLocalStorage("isDark", false); // Default to light mode
   const [isShowingSplashAnimation, setIsShowingSplashAnimation] =
     useState(true);
 
